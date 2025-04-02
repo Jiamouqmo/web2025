07e6805bd4b631a7f176b6001ca9a2434f869130
Jiamouqmo <jay20061211@gmail.com>
2025-04-02 11:54:53 +0800

    Create msgboard.sql
    2025.4.2

---
+++ msgboard.sql
@@ -0,0 +1,7 @@
+create database msgboard;
+use msgboard;
+create table account (
+        idno int primary key auto_increment,
+        name varchar(64) not null
+);
+insert into account set name="user";
