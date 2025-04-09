d857155295cbfe319287e42fb6d213060042fbab
jiamouqmo <jay20061211@gmail.com>
2025-04-09 11:57:25 +0800

    php mysqli

--- 0402/msgboard.sql
+++ 0402/msgboard.sql
@@ -1,7 +1,10 @@
+DROP database IF EXISTS msgboard;
 create database msgboard;
 use msgboard;
 create table account (
         idno int primary key auto_increment,
-        name varchar(64) not null
+        name varchar(64) not null,
+        pass varchar(256) not null
 );
-insert into account set name="user";
+
+
---
+++ 0409/account_table.php
@@ -0,0 +1,14 @@
+<table border=1>
+    <tr>
+        <th>ID</th>
+        <th>name</th>
+    </tr>
+    <?php
+    $mysqli = new mysqli("localhost","root","","msgboard");
+    $result = $mysqli->query("SELECT * FROM account");
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+    foreach($rows as $row){
+        printf("<tr><td>%d</td><td>%s</td></tr>\n",$row["idno"],$row["name"]);
+    }    
+    ?>
+</table>
